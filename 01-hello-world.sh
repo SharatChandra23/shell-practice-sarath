9c7e43fce37c905edf428d4a4a2610f0328e8b04
SharatChandra23 <sarat.maganti23@gmail.com>
2026-05-28 16:26:32 +0530

    Hello word commit

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This should be teh first line in shell, path of the interpreter which will execute the script. In this case, it is bash.  
 # This is a simple script to print "Hello World, Sarath!" to the console.
 
 echo "Hello World, Sarath!"
