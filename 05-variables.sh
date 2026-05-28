5d2ac38f5e59ae8bd4e0a012820ae9f38630784b
SharatChandra23 <sarat.maganti23@gmail.com>
2026-05-28 18:13:33 +0530

    conversion sh files changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,3 +2,11 @@
 
 TIMESTAMP=$(date +%s) # this command will give you the current timestamp in seconds since January 1, 1970 (also known as Unix epoch time)
 echo "Current timestamp: $TIMESTAMP"
+
+START_TIME=$(date +%s)
+sleep 10
+END_TIME$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script executed in $TOTAL_TIME in seconds"
