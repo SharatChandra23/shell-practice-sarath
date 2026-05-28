5f182ff8e4e2b1537828d8616b5d09fa809ce108
SharatChandra23 <sarat.maganti23@gmail.com>
2026-05-28 18:07:53 +0530

    conversion sh files changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,3 +4,7 @@ echo "Please enter your username::"
 read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in  this veriables
 
 echo "Username is $USER_NAME"
+
+echo "Please enter your password::"
+read -s PASSWORD
+echo "Password is $PASSWORD"
