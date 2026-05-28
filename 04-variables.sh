8fc2fd85b91bfdffebd43cf07e70862b0d39ed24
SharatChandra23 <sarat.maganti23@gmail.com>
2026-05-28 18:11:28 +0530

    conversion sh files changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,5 +6,5 @@ read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stor
 echo "Username is $USER_NAME"
 
 echo "Please enter your password::"
-read -s PASSWORD
+read -s PASSWORD # here -s is used to hide the password when you enter in terminal, whatever you enter in terminal stores in  this veriables
 echo "Password is $PASSWORD"
---
+++ 05-variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+TIMESTAMP=$(date +%s) # this command will give you the current timestamp in seconds since January 1, 1970 (also known as Unix epoch time)
+echo "Current timestamp: $TIMESTAMP"
