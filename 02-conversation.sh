3b0acc7367dd884aca809f2fd245c272ae6a72af
SharatChandra23 <sarat.maganti23@gmail.com>
2026-05-28 18:06:10 +0530

    conversion sh files changes

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PERSON1=($1)
-PERSON2=($2)
-
-echo "$PERSON1: Hey $PERSON2, we are going to blast you!"
-echo "$PERSON2: Yes, please. you are welcome"
-echo "$PERSON1: Hey $PERSON1 I am serious"
-echo "$PERSON2: Hey $PERSON1, we are also serious"
+echo "Trump: Hey Iran, we are going to blast you!"
+echo "Iran: Oh really? We will blast you back!"
+echo "Trump: Oh no, we will blast you harder!"
+echo "Iran: We will blast you harder than you can imagine!"
+echo "Trump: We will blast you so hard that you won't know what hit you!"
+echo "Iran: We will blast you so hard that you won't know what hit you either!"
+echo "Trump: We will blast you so hard that you will regret ever crossing us!"
+echo "Iran: We will blast you so hard that you will regret ever crossing us too!"
