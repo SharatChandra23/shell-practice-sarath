178a64ad70011b15d9d685203f74ac993cc72d86
SharatChandra23 <sarat.maganti23@gmail.com>
2026-05-28 17:53:54 +0530

    conversion sh files changes

---
+++ 02-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Trump: Hey Iran, we are going to blast you!"
+echo "Iran: Oh really? We will blast you back!"
+echo "Trump: Oh no, we will blast you harder!"
+echo "Iran: We will blast you harder than you can imagine!"
+echo "Trump: We will blast you so hard that you won't know what hit you!"
+echo "Iran: We will blast you so hard that you won't know what hit you either!"
+echo "Trump: We will blast you so hard that you will regret ever crossing us!"
+echo "Iran: We will blast you so hard that you will regret ever crossing us too!"
